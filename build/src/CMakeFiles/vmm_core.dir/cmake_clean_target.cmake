daadf05eb2e8d861280901d1b3cfea4d235eddf0
Vinícius Souza <vinidisz@gmail.com>
2026-08-04 07:26:03 -0300

    Remove build directory from repository

--- build/src/CMakeFiles/vmm_core.dir/cmake_clean_target.cmake
+++
@@ -1,3 +0,0 @@
-file(REMOVE_RECURSE
-  "libvmm_core.a"
-)
--- build/tests/CTestTestfile.cmake
+++
@@ -1,8 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: C:/Users/vinid/Downloads/virtual-memory-manager/tests
-# Build directory: C:/Users/vinid/Downloads/virtual-memory-manager/build/tests
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-add_test("BasicSanityTest" "C:/Users/vinid/Downloads/virtual-memory-manager/build/tests/vmm_test_basic.exe")
-set_tests_properties("BasicSanityTest" PROPERTIES  _BACKTRACE_TRIPLES "C:/Users/vinid/Downloads/virtual-memory-manager/tests/CMakeLists.txt;5;add_test;C:/Users/vinid/Downloads/virtual-memory-manager/tests/CMakeLists.txt;0;")
